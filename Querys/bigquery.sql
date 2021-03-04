03d29f8a964b0e8327941031f08a236739b70a2f
Daniel Jiménez <cjimenez187@aol.com>
2021-03-03 21:11:53 -0500

    Pokemon Data

--- Querys/bigquery.sql
+++ Querys/bigquery.sql
@@ -3,3 +3,32 @@
 select name,hp,attack,sp_defense,sp_attack ,type1
 from robotic-augury-283918.pokemon.pokemon_completo
 
+-- Haciendo algunas operaciones de estadística --
+
+select type1 as Type,  
+avg(attack) as Average,
+min(attack) as MIN,
+max(attack) as MAX
+from robotic-augury-283918.pokemon.pokemon_clase
+group by (Type)
+
+-- Un pequeño filtro --
+select type1 as Type, 
+avg(attack) as Average,
+min(attack) as MIN,
+max(attack) as MAX
+from robotic-augury-283918.pokemon.pokemon_clase
+WHERE name!="Walrein"
+group by (Type)
+
+
+-- Ordenando la tabla --
+
+select type1 as Type, 
+avg(attack) as Average,
+min(attack) as MIN,
+max(attack) as MAX
+from robotic-augury-283918.pokemon.pokemon_clase
+WHERE name!="Walrein"
+group by (Type)
+order by (-Average)
